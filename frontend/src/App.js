c2ac55ace1af671c43c7e34f5717f81e51dae46d
John Ahn <smileajw1004@gmail.com>
2020-07-31 22:01:23 +0900

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,7 +57,7 @@ function App() {
               onChange={changeHandler}
               value={value}
             />
-            <button type="submit">확인</button>
+            <button type="submit">확인.</button>
           </form>
         </div>
       </header>

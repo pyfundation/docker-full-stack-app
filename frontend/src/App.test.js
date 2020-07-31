93cd2b15bcd82ea5c7f03fbb9774d015636fb428
John Ahn <smileajw1004@gmail.com>
2020-07-31 21:55:23 +0900

    changed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  
 });

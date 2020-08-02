6ad7be847fb581c6435ac4dcf3e3e60ad886ab98
John Ahn <smileajw1004@gmail.com>
2020-08-02 13:35:00 +0900

    changed

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: 'mysql',
+    user: 'root',
+    password: 'johnahn',
+    database: 'myapp',
+    port: 3306
 });
 exports.pool = pool;

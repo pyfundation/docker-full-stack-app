1aae53055d17cf70e47dafb8612313b787f4c105
John Ahn <smileajw1004@gmail.com>
2020-08-02 12:30:25 +0900

    travis ci deploy Added

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,21 @@ const app = express();
 // json 형대토 오는 요청의 본문을 해석해줄수있게 등록
 app.use(bodyParser.json());
 
-//테이블 생성하기 
-// db.pool.query(`CREATE TABLE lists (
-//     id INTEGER AUTO_INCREMENT,
-//     value TEXT, 
-//     PRIMARY KEY (id)
-// )`, (err, results, fileds) => {
-//     console.log('results', results)
-// })
+// 테이블 생성하기 
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT, 
+    PRIMARY KEY (id)
+)`, (err, results, fileds) => {
+    console.log('results', results)
+})
+
+
+//DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기 
+app.get('/api/hello', function (req, res) {
+    //데이테베이스에서 모든 정보 가져오기 
+   res.status(200).send('good')
+})
 
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,15 @@ import axios from 'axios';
 
 function App() {
 
+
+  useEffect(() => {
+    //여기서 데이터베이스에 있는 값을 가져온다.
+    axios.get('/api/hi')
+      .then(response => {
+        console.log('response', response)
+      })
+  }, [])
+
   useEffect(() => {
     //여기서 데이터베이스에 있는 값을 가져온다.
     axios.get('/api/values')

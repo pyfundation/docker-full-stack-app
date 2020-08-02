b2c7e1d72b1875fe6304867a459ff9ee1f6fd05b
John Ahn <smileajw1004@gmail.com>
2020-08-02 12:42:50 +0900

    travis ci deploy Added

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,6 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_PORT
+    database: process.env.MYSQL_DATABASE
 });
 exports.pool = pool;
